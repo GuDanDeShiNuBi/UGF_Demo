9181417503698629220bdba8a16abd97e588bfa4
sure <1175316102@qq.com>
2022-12-16 14:49:32 +0800

    resources commit -20221216

---
+++ Assets/GameFramework/Scripts/Runtime/Utility/FileLogHelper.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameFramework;
+using System;
+using System.IO;
+using UnityGameFramework.Runtime;
+using System.Text;
+
+namespace Tutorial
+{
+    internal class FileLogHelper : DefaultLogHelper
+    {
+        private readonly string CurrentLogPath = Utility.Path.GetRegularPath(Path.Combine(Application.persistentDataPath, "current.log"));
+        private readonly string PreviousLogPath = Utility.Path.GetRegularPath(Path.Combine(Application.persistentDataPath, "previous.log"));
+
+        public FileLogHelper()
+        {
+            Application.logMessageReceived += OnLogMessageReceived;
+
+            try
+            {
+                if (File.Exists(PreviousLogPath))
+                {
+                    File.Delete(PreviousLogPath);
+                }
+
+                if (File.Exists(CurrentLogPath))
+                {
+                    File.Move(CurrentLogPath, PreviousLogPath);
+                }
+            }
+            catch
+            {
+            }
+        }
+
+        private void OnLogMessageReceived(string logMessage, string stackTrace, LogType logType)
+        {
+            string log = Utility.Text.Format("[{0}][{1}] {2}{4}{3}{4}", DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), logType.ToString(), logMessage ?? "<Empty Message>", stackTrace ?? "<Empty StackTrace>", Environment.NewLine);
+            try
+            {
+                File.AppendAllText(CurrentLogPath, log, Encoding.UTF8);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
+
